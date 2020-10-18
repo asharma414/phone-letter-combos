1f59146687afc523a6ae43381f8ae28d819ca0cb
asharma414 <abhi.sharm.414@gmail.com>
2020-10-18 14:29:15 -0400

    different solution attempt

--- solution.py
+++ solution.py
@@ -1,16 +1,21 @@
 class Solution(object):
-    def letterCombinations(self, digits):
-        def helper(digits, digit, partial, result):
-            if digit == len(digits):
-                result.append(partial)
-            else:
-                for i in range(len(mapping[digits[digit]])):
-                    c = mapping[digits[digit]][i]
-                    partial += c
-                    helper(digits, digit+1, partial, result)
+   def letterCombinations(self, digits):
+       def helper(digits, characters, result, current_string="", current_level=0):
+           if current_level == len(digits):
+               result.append(current_string)
+               return
+            for i in characters[int(digits[current_level])]:
+                solve(digits, characters, result, current_string+i, current_level+1)
+        if len(digits) == 0:
+            return []
+        characters = {2: "abc", 3: "def", 4: "ghi", 5: "jkl",
+                    6: "mno", 7: "pqrs", 8: "tuv", 9: "wxyz"}
         result = []
-        partial = ''
-        mapping = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
-                   '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-        helper(digits, 0, partial, result)
+        solve(digits, characters, result)
         return result
+
+
+
+
+ob1 = Solution()
+print(ob1.letterCombinations("37"))
